b88ccc1d7d28122d2c4bc21de932169a20013f53
Abdul Jawad <jawadefaj006@gmail.com>
2017-01-09 19:59:04 +0600

    fireball

--- BouncingBall/Assets/jawad/script/FireBall.cs
+++ BouncingBall/Assets/jawad/script/FireBall.cs
@@ -6,9 +6,10 @@ public class FireBall : MonoBehaviour, iBall {
 
 	private int id = 4;
 	private Vector2 position;
-	private Vector3 movedirection;
-	private float movespeed = 0.0f;
+	public Vector3 movedirection;
+	private float movespeed = 0.06f;
 	public bool Thrown = false;
+	public float blustdistance;
 
 
 	public void SetMoveDirection(Vector3 dir){
@@ -54,9 +55,33 @@ public class FireBall : MonoBehaviour, iBall {
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
+		iBall i = other.GetComponent<iBall>();
+		if (this.Thrown == true)
+		{
+			print ("inside if");
+			if (i != null)
+			{
+				RaycastHit2D rr1 = Physics2D.Raycast (other.transform.position + new Vector3(0.0f, 0.8f, 0.0f), new Vector2 (0.0f, 1.0f), blustdistance);
+				print ("rr1 "+rr1.collider.name);
+				if(rr1 != null)
+				Destroy (rr1.collider.gameObject);
+
+				RaycastHit2D rr2 = Physics2D.Raycast (other.transform.position + new Vector3(0.8f, 0.0f, 0.0f), new Vector2 (1.0f, 0.0f), blustdistance);
+				print ("rr2 "+rr2.collider.name);
+				if(rr1 != null)
+				Destroy (rr2.collider.gameObject);
+
+				RaycastHit2D rr3 = Physics2D.Raycast (other.transform.position + new Vector3(-0.8f, 0.0f, 0.0f), new Vector2 (-1.0f, 0.0f), blustdistance);
+				print ("rr3 "+rr3.collider.name);
+				if(rr1 != null)
+				Destroy (rr3.collider.gameObject);
+			}
+
+
+		}
 
 		//print (this.transform.position);
-		iBall i = other.GetComponent<iBall>();
+
 		print ("on trigger fire");
 		if(i != null){
 			if (i.isThrown)
