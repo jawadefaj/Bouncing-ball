09c2880f8dccbc83d35c3493305434e574bd30e5
Abdul Jawad <jawadefaj006@gmail.com>
2017-01-20 14:22:08 +0600

    SplitBall done

--- BouncingBall/Assets/jawad/script/SplitBall.cs
+++ BouncingBall/Assets/jawad/script/SplitBall.cs
@@ -6,9 +6,14 @@ public class SplitBall : MonoBehaviour, iBall {
 
 	private int id = 5;
 	private Vector2 position;
-	private Vector3 movedirection;
-	private float movespeed = 0.0f;
+
+	public Vector3 movedirection;
+	public float movespeed = 0.0f;
 	public bool Thrown = false;
+
+	private float instantiatetime = 0.0f;
+	public float splittime = 0.0f;
+	public static bool split = true;
 
 	public void SetMoveDirection(Vector3 dir){
 		movedirection.x = dir.x;
@@ -44,12 +49,32 @@ public class SplitBall : MonoBehaviour, iBall {
 	}
 	// Use this for initialization
 	void Start () {
-		
+		instantiatetime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		this.gameObject.transform.position = Vector2.MoveTowards (this.gameObject.transform.position, this.gameObject.transform.position + movedirection, movespeed);
+		if ((Time.time - instantiatetime) > splittime && split)
+		{
+			Vector3 normal = new Vector3 ( 0.0f, 1.0f, 0.0f).normalized;
+			float product = Vector3.Dot ( - movedirection, normal);
+			Vector3 pro = 2 * product * normal;
+			Vector3 newmovedirection = - movedirection - pro;
+			newmovedirection.Normalize();
+			GameObject newball = Instantiate (this.gameObject);
+			iBall ib = newball.GetComponent<iBall> ();
+			ib.isThrown = true;
+			ib.SetMoveDirection (newmovedirection);
+			this.transform.localScale *= 0.7f; 
+			newball.transform.localScale = this.transform.localScale;
+			ib.SetSpeed (movespeed);
+			split = false;
+		}
+
+
+
+
 	}
 	void OnTriggerEnter2D(Collider2D other){
 
@@ -57,7 +82,7 @@ public class SplitBall : MonoBehaviour, iBall {
 		iBall i = other.GetComponent<iBall>();
 		print ("on trigger split");
 		if(i != null){
-			if (i.isThrown)
+			if (i.isThrown && !this.isThrown)
 			{
 				Destroy (other.gameObject);
 				Destroy (this.gameObject);
